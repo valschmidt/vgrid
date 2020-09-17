f4ea50f4435ec9c9241d818cdf19e51b2f6c12df
Val E. Schmidt <vschmidt@ccom.unh.edu>
2020-09-17 16:59:48 -0400

    Added original matlab version of vgrid.

---
+++ @vgrid/zz.m
@@ -0,0 +1,8 @@
+function z = zz(g)
+%% A function to calculate and return z values from an incremental grid.
+%
+% Val Schmidt
+% Center for Coastal and Ocean Mapping
+% University of New Hampshire
+% Copyright 2010-2020, All rights reserved.
+z = g.zw./g.ww;
